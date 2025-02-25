b384eb730b88ec7b5f5f930029b0f666fb03a5e1
balbarak <balbarak@live.com>
2025-02-25 21:46:51 +0300

    now we can change language !

--- src/UnoAppTemplate/ViewModels/BaseViewModel.cs
+++ src/UnoAppTemplate/ViewModels/BaseViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnoAppTemplate.Helpers;
+using UnoAppTemplate.Resx;
 
 namespace UnoAppTemplate.ViewModels;
 
@@ -12,6 +13,5 @@ public class BaseViewModel : BindableObjectBase
 
     private bool _isBusy;
     public bool IsBusy { get => _isBusy; set => SetValue(ref _isBusy, value); }
-
 
 }
--- src/UnoAppTemplate/Views/SettingsPage.xaml.cs
+++ src/UnoAppTemplate/Views/SettingsPage.xaml.cs
@@ -6,9 +6,9 @@ public sealed partial class SettingsPage : Page
     public SettingsViewModel ViewModel => DataContext as SettingsViewModel;
     public SettingsPage()
     {
-        this.InitializeComponent();
-
         DataContext = App.GetService<SettingsViewModel>();
+
+        this.InitializeComponent();
 
         SetCurrentLangauge();
     }
